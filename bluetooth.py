4234a94fa22ab09e88a442fda73f37070fda8de1
VK <67276637+VK11-7@users.noreply.github.com>
2024-03-26 12:17:05 +0530

    Update bluetooth.py

--- bluetooth.py
+++ bluetooth.py
@@ -1,3 +1,7 @@
 import bluetooth
-nearby_devices = bluetooth.discover_devices()
-print("Found %d devices" % len(nearby_devices))
+
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("Found {} devices.".format(len(nearby_devices)))
+
+for addr, name in nearby_devices:
+    print("  {} - {}".format(addr, name))
